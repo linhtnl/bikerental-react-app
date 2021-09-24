2a811da2af9c01809cbe0364af7e40795ac4d40e
Ngoc Nguyen <nguyenngoc0298@gmail.com>
2021-09-24 09:03:50 +0700

    NgocNM 1 commit

---
+++ src/views/Examples/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+    render(){
+        return(
+            <div>Rental Bike Application</div>
+        )
+    }
+
+}
+
+export default MyComponent;
