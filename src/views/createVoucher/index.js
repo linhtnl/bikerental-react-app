b9e2ae273cd4ca1f0cc5cd03815347716ce52aea
Ngoc Nguyen <nguyenngoc0298@gmail.com>
2021-10-08 01:31:57 +0700

    commit createVoucher

--- src/views/createVoucher/index.js
+++ src/views/createVoucher/index.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import {
+  CAvatar,
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CForm,
+  CFormCheck,
+  CFormInput,
+  CFormLabel,
+  CFormSelect,
+  CRow,
+} from '@coreui/react'
+import React, { useEffect, useState } from 'react'
+import getUserAPI from 'src/api/getUserAPI'
+import { DocsExample } from 'src/components'
+import styles from './index.scss'
 
 const CreateVoucher = () => {
-  return <div>Create voucher page</div>
+  return (
+    <>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardBody>
+            <CForm className="row g-3">
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputVoucherName">Voucher Name</CFormLabel>
+                <CFormInput id="inputVoucherName" placeholder="Voucher Name" value="" />
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputDescription">Description</CFormLabel>
+                <CFormInput
+                  type="text"
+                  id="inputDescription"
+                  placeholder="Discount 20% off total purchase, maximun is 20000VND"
+                  value=""
+                />
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputDiscountPercent">Discount Percent</CFormLabel>
+                <CFormInput
+                  type="text"
+                  id="inputDiscountPercent"
+                  placeholder="10%, 20%,  30% ..."
+                  value=""
+                />
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputMaxAmount">Max Amount</CFormLabel>
+                <CFormInput type="text" id="inputMaxAmount" placeholder="50.000 VNĐ" value="" />
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputStartDate">Starting Date</CFormLabel>
+                <CFormInput id="inputStartDay" type="date" placeholder="dd/mm/yyy" value="" />
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputEndDate">End Date</CFormLabel>
+                <CFormInput id="inputEndDate" type="date" placeholder="dd/mm/yyy" value="" />
+              </CCol>
+              <CCol md={6}>
+                <CFormLabel htmlFor="inputRemain">Remain</CFormLabel>
+                <CFormInput type="text" id="inputRemain" placeholder="100" value="" />
+              </CCol>
+              <CCol xs={12}>
+                <p align="center">
+                  <CButton id="createButton" type="submit">
+                    Create
+                  </CButton>
+                </p>
+              </CCol>
+            </CForm>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </>
+  )
 }
 
 export default CreateVoucher
